0e47b65b946b50ae10b7003310ebcefac79a627e
Enrico <encollini@gmail.com>
2019-06-02 10:48:05 +0200

    fixone

--- src/views/BigMap.vue
+++ src/views/BigMap.vue
@@ -43,6 +43,14 @@
 </template>
 
 <script>
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
+
 //importing json marker file
 import jsn from "./../assets/ecostations";
 //importing map leaflet stuff
